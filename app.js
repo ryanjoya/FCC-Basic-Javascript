609a066af19d16afe43361b529abfa4bf78b2eeb
Ryan <ryebread23@users.noreply.github.com>
2016-06-23 22:12:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 
-// Initialize these three variables
-var a = 5;
-var b = 10;
-var c = "I am a";
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
 
-// Do not change code below this line
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
