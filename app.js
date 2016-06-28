3359025b1c2530a889861cbfda7d87dac067f55f
Ryan <ryebread23@users.noreply.github.com>
2016-06-27 23:50:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
-var a = 5;
-var b = 12;
-var c = 4.6;
+var a = 48;
+var b = 108;
+var c = 33;
 
 // Only modify code below this line
 
-a = a *= 5;
-b = b *= 3;
-c = c *= 10;
-
+a = a /= 12;
+b = b /= 4;
+c = c /= 11;
 
