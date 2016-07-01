4cbe790153e0b7a487de02140bd4d3d56b539d23
Ryan <ryebread23@users.noreply.github.com>
2016-06-30 23:55:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 
-// Example
-var firstName = "Alan";
-var lastName = "Turing";
-
-// Only change code below this line
-var myFirstName = "Robert";
-var myLastName = "Prot";
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
 
