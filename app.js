5377f37538baa2353df04345fa05d841e520903b
Ryan <ryebread23@users.noreply.github.com>
2016-07-01 23:59:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 
-var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+
+
 
 
