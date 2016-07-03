37b8bb4365de78a56fb6e601f015e5d33f96f021
Ryan <ryebread23@users.noreply.github.com>
2016-07-03 00:29:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-
-var myStr = 'Here is a backslash: \\. \n \t \t Here is a new line with two tabs.'; // Change this line
-
-
+var myStr = "Here is a backslash: \\.\n\t\tHere is a new line with two tabs."; // Change this line
