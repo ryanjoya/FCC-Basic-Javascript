7627fc92702a006739244293cff084151d917938
Ryan <ryebread23@users.noreply.github.com>
2016-07-12 23:51:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 // Example
 var firstName = "Ada";
-var lastLetterOfFirstName = firstName[firstName.length - 1];
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
 
 // Setup
 var lastName = "Lovelace";
 
-// Only change code below this line.
-var lastLetterOfLastName = lastName[lastName.length -1];
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
 
 
