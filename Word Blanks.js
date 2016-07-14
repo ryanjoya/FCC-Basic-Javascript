43eb6e749bdb39050cc73dc96da8279d1751247e
Ryan <ryebread23@users.noreply.github.com>
2016-07-13 23:39:03 -0500

    Create Word Blanks.js

---
+++ Word Blanks.js
@@ -0,0 +1,12 @@
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result += ""+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+"";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("biscuit", "dry", "ate", "slowly");
