27cf7c821f8c550926d8060e8a2a1c7f17f65000
Ryan <ryebread23@users.noreply.github.com>
2016-07-22 17:12:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 // Example
-var ourArray = ["Stimpson", "J", ["cat"]];
-removedFromOurArray = ourArray.shift();
-// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); 
+// ourArray now equals ["Happy", "J", "cat"]
 
 // Setup
 var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
 
 // Only change code below this line.
-var removedFromMyArray = myArray.shift();
-
+myArray.unshift(["Paul", 35]);
 
