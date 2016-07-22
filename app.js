062dedfafbee8fd6823c1af01479d2aaa49dd70f
Ryan <ryebread23@users.noreply.github.com>
2016-07-21 23:39:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 
 // Example
-var ourArray = [1,2,3];
-var removedFromOurArray = ourArray.pop(); 
-// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+var ourArray = ["Stimpson", "J", ["cat"]];
+removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
 
 // Setup
-var myArray = [["John", 23], ["cat", 2]];
+var myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line.
-var removedFromMyArray = myArray.pop();
+var removedFromMyArray = myArray.shift();
 
 
