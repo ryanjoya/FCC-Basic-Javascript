2a094b9f9297c2b703b18b9df3b8368882ef23f4
Ryan <ryebread23@users.noreply.github.com>
2016-07-24 23:46:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 
 // Example
-function ourReusableFunction() {
-  console.log("Heyya, World");
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(x, y)  {
+  console.log(x + y);
 }
 
-ourReusableFunction();
-
-// Only change code below this line
-function reusableFunction(){
-  console.log("Hi World");
-}
-
-reusableFunction();
+functionWithArgs(20, 22);
