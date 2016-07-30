f4cdfa4e5d657d0aba4363074697077bea0d7916
Ryan <ryebread23@users.noreply.github.com>
2016-07-29 23:51:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 
 // Example
-function minusSeven(num) {
-  return num - 7;
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
 }
 
 // Only change code below this line
-function timesFive(num){
-  return num * 5;
-}
+processed = processArg(7);
 
