6c04fc9361a4ed0f2acc2451961fb617f3ea9e39
Ryan <ryebread23@users.noreply.github.com>
2016-07-31 22:35:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 
-// Example
-var changed = 0;
+function welcomeToBooleans() {
 
-function change(num) {
-  return (num + 5) / 3;
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
 }
-
-changed = change(10);
-
-// Setup
-var processed = 0;
-
-function processArg(num) {
-  return (num + 3) / 5;
-}
-
-// Only change code below this line
-processed = processArg(7);
-
