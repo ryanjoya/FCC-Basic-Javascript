e78234be266c7e474948c46a2f39981fc4b779ce
Ryan <ryebread23@users.noreply.github.com>
2016-08-02 23:38:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 // Setup
-function testEqual(val) {
-  if (val == 12) { // Change this line
+function testStrict(val) {
+  if (val === 7) { // Change this line
     return "Equal";
   }
   return "Not Equal";
 }
 
 // Change this value to test
-testEqual(12);
+testStrict(7);
