a5d46df346d8d9650c66adca6901ec2eb79d1bb6
Ryan <ryebread23@users.noreply.github.com>
2016-08-04 23:26:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 
 // Setup
-function testNotEqual(val) {
-  if (val != 99) { // Change this line
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+  
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
     return "Not Equal";
   }
   return "Equal";
 }
 
 // Change this value to test
-testNotEqual(10);
+testStrictNotEqual(17);
