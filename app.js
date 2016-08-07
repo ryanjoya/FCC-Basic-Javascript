5e98168aabba7299cab71f7aac1cf3d1dd71f8f0
Ryan <ryebread23@users.noreply.github.com>
2016-08-06 21:45:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
-function testGreaterThan(val) {
-  if (val > 100) {  // Change this line
-    return "Over 100";
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
   }
   
-  if (val > 10) {  // Change this line
-    return "Over 10";
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
   }
 
-  return "10 or Under";
+  return "9 or Under";
 }
 
 // Change this value to test
-testGreaterThan(10);
+testGreaterOrEqual(10);
