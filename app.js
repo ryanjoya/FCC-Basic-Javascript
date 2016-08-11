c8cbe38a7b04b1d79e1bc2717316c6e663db7d4d
Ryan <ryebread23@users.noreply.github.com>
2016-08-11 00:07:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ function testLogicalAnd(val) {
   if (val <= 50 && val >= 25) {
       return "Yes";
     }
-  }
 
   // Only change code above this line
   return "No";
 }
 
 // Change this value to test
-testLogicalAnd(10);
+testLogicalAnd(27);
