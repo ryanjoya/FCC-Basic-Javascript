9e46b107acc73f8e59d46c189f89dca742df75e7
Ryan <ryebread23@users.noreply.github.com>
2016-08-13 22:10:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 
-function testElse(val) {
-  var result = "";
-  // Only change code below this line
-  
-  if (val > 5) {
-    result = "Bigger than 5";
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
   }
   
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
   else {
-    result = "5 or Smaller";
+    return "Between 5 and 10";
   }
   
-  // Only change code above this line
-  return result;
 }
 
 // Change this value to test
-testElse(4);
+testElseIf(7);
 
