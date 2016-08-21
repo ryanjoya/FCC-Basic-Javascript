fc3da6260ecc7a120d1035efffa9ea3fd457632e
Ryan <ryebread23@users.noreply.github.com>
2016-08-20 22:49:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 
-function switchOfStuff(val) {
+function sequentialSizes(val) {
   var answer = "";
   // Only change code below this line
-  switch (val) {
-    case "a":
-      answer = "apple";
+  switch(val)  {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
       break;
-    case "b":
-      answer = "bird";
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
       break;
-    case "c":
-      answer = "cat";
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
       break;
-    default:
-      return "stuff";
-  
   }
+  
+  
   // Only change code above this line  
   return answer;  
 }
 
 // Change this value to test
-switchOfStuff("b");
+sequentialSizes(4);
 
