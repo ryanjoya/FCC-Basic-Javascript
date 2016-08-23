7e6cc9f91d92c79b641e76e923e6917378bf0fe6
Ryan <ryebread23@users.noreply.github.com>
2016-08-23 18:45:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
-function isLess(a, b) {
-  // Fix this code
-  return a < b;
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change these values to test
-isLess(10, 15);
+// Change values below to test your code
+abTest(2,2);
