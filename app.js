55e625df563b5e374f0913dff13a0708c9b554ee
Ryan <ryebread23@users.noreply.github.com>
2016-08-25 22:45:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
-// Setup
-function abTest(a, b) {
-  // Only change code below this line
-  if (a < 0 || b < 0) {
-    return undefined;
-  }
-  
-  // Only change code above this line
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
 
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
-}
+// Only change code below this line.
 
-// Change values below to test your code
-abTest(2,2);
+var myDog = {
+  "name": "Joe",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["balls", "papers"]
+};
