22757308e7445306bfddee3965a04c79ddfda988
Ryan <ryebread23@users.noreply.github.com>
2016-09-04 18:28:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 
-var myMusic = [
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [ 
-      "CS", 
-      "8T", 
-      "LP" ],
-    "gold": true
-  },
-  // Add record here
-  {
-    "artist": "Alicia Keys",
-    "title": "Songs in A Minor",
-    "release_year": 2001,
-    "formats":  [
-      "CD",
-      "MP3",
-      "LP" ],
-    "gold": true
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
   }
-];
+};
+
+// Only change code below this line
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
 
