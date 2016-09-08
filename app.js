e6b8dbcaa89384d862455cef107acd94f8532565
Ryan <ryebread23@users.noreply.github.com>
2016-09-08 14:59:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Example
 var ourArray = [];
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 10; i += 2) {
   ourArray.push(i);
 }
 
@@ -10,7 +10,7 @@ for (var i = 0; i < 5; i++) {
 var myArray = [];
 
 // Only change code below this line.
-for (var i = 1; i < 6; i++)  {
+for (var i = 1; i < 10; i += 2)  {
   myArray.push(i);
 }
 
