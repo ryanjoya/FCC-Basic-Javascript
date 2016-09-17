d91592f0ddf3ee1d8e90b9237d36681636b4b19b
Ryan <ryanjoya@users.noreply.github.com>
2016-09-16 23:46:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
-function randomFraction() {
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
 
   // Only change code below this line.
+  
+  return Math.floor(Math.random() * 10);
 
-  return Math.random();
-
-  // Only change code above this line.
 }
