2237c6f5ce370bc323bc5370cf50111d213d3444
Ryan <ryanjoya@users.noreply.github.com>
2016-09-20 23:36:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 
 // Setup
-var testString = "Ada Lovelace and Charles Babbage designed the first computer and the software that would have run on it.";
-
-// Example
-var expressionToGetSoftware = /software/gi;
-var softwareCount = testString.match(expressionToGetSoftware).length;
-  
+var testString = "There are 3 cats but 4 dogs.";
 
 // Only change code below this line.
 
-var expression = /and/gi;  // Change this Line
+var expression = /\d+/g;  // Change this line
 
 // Only change code above this line
 
 // This code counts the matches of expression in testString
-var andCount = testString.match(expression).length;
-
+var digitCount = testString.match(expression).length;
