88ddd7ee0c1395ad3a4e028d9579d3207173dc14
Ryan <ryanjoya@users.noreply.github.com>
2016-09-21 23:17:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 // Setup
-var testString = "There are 3 cats but 4 dogs.";
+var testString = "How many spaces are there in this sentence?";
 
 // Only change code below this line.
 
-var expression = /\d+/g;  // Change this line
+var expression = /\s+/g;  // Change this line
 
 // Only change code above this line
 
 // This code counts the matches of expression in testString
-var digitCount = testString.match(expression).length;
+var spaceCount = testString.match(expression).length;
