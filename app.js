e8131ea761758bc7503f1d7876a77405614c332c
Ryan <ryanjoya@users.noreply.github.com>
2016-09-22 23:56:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 // Setup
-var testString = "How many spaces are there in this sentence?";
+var testString = "How many non-space characters are there in this sentence?";
 
 // Only change code below this line.
 
-var expression = /\s+/g;  // Change this line
+var expression = /\S/g;  // Change this line
 
 // Only change code above this line
 
 // This code counts the matches of expression in testString
-var spaceCount = testString.match(expression).length;
+var nonSpaceCount = testString.match(expression).length;
